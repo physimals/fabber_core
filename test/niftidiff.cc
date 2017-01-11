2a8a1461b2ef1c71d66ea797aa4939aeb8651942
Martin Craig <martin.craig@eng.ox.ac.uk>
2017-01-11 14:45:02 +0000

    Added mask/fill value option to niftidiff

--- test/niftidiff.cc
+++ test/niftidiff.cc
@@ -13,16 +13,31 @@ using namespace std;
 
 int main(int argc, char **argv)
 {
-	if (argc != 3)
+	if ((argc < 3) || (argc > 5))
 	{
-		cout << "Usage: niftidiff <file1> <file2>" << endl;
+		cout << "Usage: niftidiff <file1> <file2> [<mask>] [--ignorezero]" << endl;
 		return 1;
 	}
 	string f1 = argv[1];
 	string f2 = argv[2];
+	string mask = "";
+	bool ignore_zero = false;
+
+	for (int i = 3; i < argc; i++)
+	{
+		if (strcmp(argv[i], "--ignorezero") == 0)
+		{
+			ignore_zero = true;
+		}
+		else
+		{
+			mask = argv[i];
+		}
+	}
 
 	NEWIMAGE::volume4D<float> d1;
 	NEWIMAGE::volume4D<float> d2;
+	NEWIMAGE::volume<float> m;
 
 	try
 	{
@@ -40,6 +55,18 @@ int main(int argc, char **argv)
 	{
 		cerr << "Error reading file " << f2 << endl;
 		return 1;
+	}
+	if (mask != "")
+	{
+		try
+		{
+			read_volume(m, mask);
+
+		} catch (...)
+		{
+			cerr << "Error reading file " << mask << endl;
+			return 1;
+		}
 	}
 
 	if (d1.xsize() != d2.xsize())
@@ -63,6 +90,25 @@ int main(int argc, char **argv)
 		return 1;
 	}
 
+	if (mask != "")
+	{
+		if (d1.xsize() != m.xsize())
+		{
+			cerr << "Mask dimension mismatch: (x): " << d1.xsize() << "!=" << m.xsize() << endl;
+			return 1;
+		}
+		if (d1.ysize() != m.ysize())
+		{
+			cerr << "Mask dimension mismatch: (y): " << d1.ysize() << "!=" << m.ysize() << endl;
+			return 1;
+		}
+		if (d1.zsize() != m.zsize())
+		{
+			cerr << "Mask dimension mismatch: (z): " << d1.zsize() << "!=" << m.zsize() << endl;
+			return 1;
+		}
+	}
+
 	for (int x = 0; x < d1.xsize(); x++)
 	{
 		for (int y = 0; y < d1.ysize(); y++)
@@ -71,11 +117,18 @@ int main(int argc, char **argv)
 			{
 				for (int t = 0; t < d1.tsize(); t++)
 				{
-					if (abs(d1(x, y, z, t) - d2(x, y, z, t)) > EPSILON)
+					if ((mask == "") || (m(x, y, z) != 0))
 					{
-						cerr << "Value mismatch (" << x << ", " << y << ", " << z << ", " << t << "): "
-								<< d1(x, y, z, t) << "!=" << d2(x, y, z, t) << endl;
-						return 1;
+						if (ignore_zero && ((d1(x, y, z, t) == 0) || (d2(x, y, z, t) == 0)))
+						{
+							continue;
+						}
+						if (abs(d1(x, y, z, t) - d2(x, y, z, t)) > EPSILON)
+						{
+							cerr << "Value mismatch (" << x << ", " << y << ", " << z << ", " << t << "): "
+									<< d1(x, y, z, t) << "!=" << d2(x, y, z, t) << endl;
+							return 1;
+						}
 					}
 				}
 			}
