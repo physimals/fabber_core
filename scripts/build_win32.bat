8b4452c9d1cffd4ada29ac6b112d056d5d3d99e4
Martin Craig <martin.craig@eng.ox.ac.uk>
2017-01-31 15:04:06 +0000

    Windows fixes

---
+++ scripts/build_win32.bat
@@ -0,0 +1,16 @@
+echo "You must run this from a Visual Studio developer commmand line"
+
+set PREFIX=%FSLDIR%
+set TYPE=Debug
+set ARCH=x64
+set BUILDDIR=build_%ARCH%_%TYPE%
+
+call "%VCINSTALLDIR%\vcvarsall" %ARCH%
+
+mkdir %BUILDDIR%
+cd %BUILDDIR%
+cmake .. -DCMAKE_INSTALL_PREFIX=%PREFIX% -DCMAKE_BUILD_TYPE=%TYPE% -G "NMake Makefiles"
+nmake
+nmake install
+cd ..
+
