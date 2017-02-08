57f06cb523d85474ddffb4b9f4a13abb378621c9
Martin Craig <martin.craig@eng.ox.ac.uk>
2017-02-08 13:14:24 +0000

    Build debug and release packages on linux

--- scripts/package.sh
+++ scripts/package.sh
@@ -3,17 +3,17 @@
 # Build distribution package for Fabber
 #
 # Usage: package.sh
-#
-# Prefix defaults to $FSLDIR if set, or $HOME if not
 
 scriptdir=`dirname $0`
 $scriptdir/build.sh release
 cd $scriptdir/../build_release
 cmake .. -DCMAKE_BUILD_TYPE=release
 make package
+cd ..
 
-scriptdir=`dirname $0`
 $scriptdir/build.sh debug
 cd $scriptdir/../build_debug
 cmake .. -DCMAKE_BUILD_TYPE=debug
 make package
+cd ..
+
