6d2d52416dd0e7931034454a1b7309b85f0fab46
Martin Craig <martin.craig@eng.ox.ac.uk>
2017-02-08 15:08:20 +0000

    Full set of build scripts

--- scripts/package.sh
+++ scripts/package.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 #
-# Build distribution package for Fabber
+# Build distribution packages
 #
 # Usage: package.sh
-#
-# Prefix defaults to $FSLDIR if set, or $HOME if not
+
+ORIGDIR=$PWD
 
 scriptdir=`dirname $0`
 $scriptdir/build.sh release
 cd $scriptdir/../build_release
-cmake .. -DCMAKE_BUILD_TYPE=release
 make package
 
 scriptdir=`dirname $0`
 $scriptdir/build.sh debug
 cd $scriptdir/../build_debug
-cmake .. -DCMAKE_BUILD_TYPE=debug
 make package
+
+cd $ORIGDIR
